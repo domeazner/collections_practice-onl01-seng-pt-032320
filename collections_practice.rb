27365fcc7f03c27a24b47f325fdc94dc71468e0c
Dominc Carolan <dominiccarolan@gmail.com>
2020-04-02 13:51:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,31 +9,28 @@ describe 'collections practice' do
     end
   end
   
-end
 
-=begin
-  # Question 2
   describe '#sort_array_desc' do
     it 'should return an array sorted in descending order' do
       expect(sort_array_desc([25, 7, 14])).to eq([25, 14, 7])
     end
   end
 
-  # Question 3
+
   describe '#sort_array_char_count' do
     it 'should return an array in ascending order sorted by the number of characters in the string' do
       expect(sort_array_char_count(["dogs", "cat", "Horses"])).to eq(["cat", "dogs", "Horses"])
     end
   end
 
-  # Question 4
+
   describe '#swap_elements' do
     it 'swap the second and third elements of an array' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
 
-  # Question 5
+
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
       expect(reverse_array([12, 4, 35])).to eq([35, 4, 12])
@@ -48,14 +45,14 @@ end
     end
   end
 
-  # Question 7
+
   describe '#find_a' do
     it 'find all words that begin with "a" in the following array' do
       expect(find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ])).to eq(["apple", "avis", "arlo", "ascot"])
     end
   end
 
-  # Question 8
+
   describe '#sum_array' do
     #for a bonus, look up the `.inject` method to get this test passing
     it 'sum all the numbers in the following array' do
@@ -63,11 +60,10 @@ end
     end
   end
 
-  # Question 9
+
   describe '#add_s' do
     it 'Add an "s" to each word in the array except for the 2nd element in the array' do
       expect(add_s(["hand","feet", "knee", "table"])).to eq(["hands","feet", "knees", "tables"])
     end
   end
 end
-=end
